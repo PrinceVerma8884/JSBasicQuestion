327f432b09be573bf63420507ae74f86c7b0ac16
PrinceVerma8884 <realuser8884@gmail.com>
2025-03-02 22:12:05 +0530

    Second Commit

---
+++ ArmstrongNumber.js
@@ -0,0 +1,15 @@
+//"Program to check that the given number is armstrong or not");
+let number = 1634;
+let numLen = (number.toString().length)
+let sum = 0;
+let temp = number;
+while(number>0){
+    let num = Math.floor(number % 10);
+    sum = sum + Math.pow(num, numLen)
+    number = Math.floor(number / 10)
+}
+if(temp == sum){
+    console.log("Its Armstong")
+}else{
+    console.log("Not Armstrong")
+}
